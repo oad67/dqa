a7b87e754cf538e9e7fcba9c4eef659bb9d6d78d
oad67 <120838621+oad67@users.noreply.github.com>
2023-10-14 01:00:10 -0500

    Update dqa.py

--- dqa.py
+++ dqa.py
@@ -9,7 +9,7 @@ from transformers import AutoModelForQuestionAnswering
 from transformers import AutoTokenizer
 
 st.set_page_config(page_title="Contextual QnA App_OA")
-#st.write("QnA App_OA")
+
 
 model_name = "deepset/roberta-base-squad2"
 
@@ -18,7 +18,7 @@ nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 C=st.text_area("Enter Context:",value="The Delaware Department of Transportation (DelDOT) is an agency of the U.S. state of Delaware. The Secretary of Transportation is Nicole Majeski. The agency was established in 1917 and has its headquarters in Dover.")
 Q=st.text_area("Enter Question:",value="Where is DelDOT located?")
 
-clicked=st.button("Answer")
+
 
 
 QA_input = {
