032a304baa8bf4bebe4e72d19fb061e24c7bb900
oad67 <120838621+oad67@users.noreply.github.com>
2023-10-14 00:44:03 -0500

    Update dqa.py

--- dqa.py
+++ dqa.py
@@ -5,13 +5,31 @@ import torch
 
 
 from transformers import pipeline
-#from transformers import AutoModelForQuestionAnswering
-#from transformers import AutoTokenizer
+from transformers import AutoModelForQuestionAnswering
+from transformers import AutoTokenizer
 
-#model_name = "deepset/roberta-base-squad2"
+model_name = "deepset/roberta-base-squad2"
+
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+
+text=st.text_area("Enter text to summarize:")
+
+clicked=st.button("Answer")
+
+
+QA_input = {
+    'question': 'Why is model conversion important?',
+    'context': 'The option to convert models between FARM and transformers gives freedom to the user and let people easily switch between frameworks.'
+}
+
+res = nlp(QA_input)
+
 
 st.set_page_config(page_title="Contextual QnA App_OA")
 st.write("QnA App_OA")
+
+if st.button("Summarize"):
+ st.write(res)
 
 #import nltk
 
