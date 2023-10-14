134d1d30d149ff3d8e9294f021f8d5ba467c8c86
oad67 <120838621+oad67@users.noreply.github.com>
2023-10-14 00:53:32 -0500

    Update dqa.py

--- dqa.py
+++ dqa.py
@@ -9,7 +9,7 @@ from transformers import AutoModelForQuestionAnswering
 from transformers import AutoTokenizer
 
 st.set_page_config(page_title="Contextual QnA App_OA")
-st.write("QnA App_OA")
+#st.write("QnA App_OA")
 
 model_name = "deepset/roberta-base-squad2"
 
